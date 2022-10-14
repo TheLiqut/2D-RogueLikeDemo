ddc6e4bb5494e14f03e389a35bfaa0f344a3f75c
AAVA <2521792014@qq.com>
2022-10-14 10:55:08 +0800

    10-14_2
    02

---
+++ Assets/Scripts/Other/TempStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TempStart : MonoBehaviour
+{
+    public void StartAsCN()
+    {
+        Global_GameManager.instance.usingLanguage = 0;
+        SceneManager.LoadScene("BaseScene");
+    }
+
+    public void StartAsEN()
+    {
+        Global_GameManager.instance.usingLanguage = 1;
+        SceneManager.LoadScene("BaseScene");
+    }
+}
